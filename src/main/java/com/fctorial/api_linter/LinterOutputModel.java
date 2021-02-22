6e94348db0a516dcfd210f28b05717219b0b1ed2
Sagar Tiwari <fctorial@gmail.com>
2021-02-23 03:26:33 +0530

    json ignoreunknown fix

--- src/main/java/com/fctorial/api_linter/LinterOutputModel.java
+++ src/main/java/com/fctorial/api_linter/LinterOutputModel.java
@@ -2,16 +2,19 @@ package com.fctorial.api_linter;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 class Position {
     public Integer line_number;
     public Integer column_number;
 }
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 class Location {
     public Position start_position;
     public Position end_position;
 }
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 class LinterWarning {
     public String message;
     public Location location;
